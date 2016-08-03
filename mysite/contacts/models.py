077015f8ef930520d5c4d977cda5f90073c3e09f
James Gordon <gordon.je@gmail.com>
2016-08-03 10:25:49 -0500

    Added field, migrated, squashed, then added another field

--- mysite/contacts/models.py
+++ mysite/contacts/models.py
@@ -5,7 +5,9 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     first_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200)
 
 class Organization(models.Model):
     name = models.CharField(max_length=200)
+    is_local = models.BooleanField(default=False)
